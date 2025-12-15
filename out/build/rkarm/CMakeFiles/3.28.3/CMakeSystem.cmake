d88952cc1aa68d8560964256c06b721b06e3dc9c
xiaotang2333333 <59103928+xiaotang2333333@users.noreply.github.com>
2025-12-15 11:56:58 +0000

    add linux cmake triplet for x64 build

--- out/build/rkarm/CMakeFiles/3.28.3/CMakeSystem.cmake
+++ out/build/rkarm/CMakeFiles/3.28.3/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "6.6.87.2-microsoft-standard-WSL2")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/dlpforRK/x64-arm64-toolchain.cmake")
+include("/home/developer/dlpforRK/x64-arm64-toolchain.cmake")
 
 set(CMAKE_SYSTEM "Linux")
 set(CMAKE_SYSTEM_NAME "Linux")
