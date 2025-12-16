25822c3d2bbecd38fd8c51368b409abd18fc52be
xiaotang2333333 <59103928+xiaotang2333333@users.noreply.github.com>
2025-12-16 04:28:14 +0000

    test

--- x64-arm64-toolchain.cmake
+++ x64-arm64-toolchain.cmake
@@ -11,9 +11,19 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv8-a")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8-a")
+set(CMAKE_SYSROOT /usr/aarch64-linux-gnu)
+set(ENV{PKG_CONFIG_SYSROOT_DIR} "${CMAKE_SYSROOT}")
+set(ENV{PKG_CONFIG_LIBDIR} "${CMAKE_SYSROOT}/usr/lib/pkgconfig:${CMAKE_SYSROOT}/usr/share/pkgconfig")
+set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_LIBDIR}")
 
 set(VCPKG_TARGET_TRIPLET arm64-linux-dynamic)
-set(VCPKG_HOST_TRIPLET arm64-linux-dynamic)
+set(VCPKG_HOST_TRIPLET  arm64-linux-dynamic)
+
+if(DEFINED ENV{VCPKG_ROOT} AND EXISTS "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+    include("$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+else()
+    message(FATAL_ERROR "VCPKG_ROOT is not set or vcpkg toolchain file not found at: $ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+endif()
 
 if(NOT CMAKE_BUILD_TYPE)
     set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build" FORCE)
