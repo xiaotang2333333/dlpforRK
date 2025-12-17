b96f5370def6ce7fe9cc3e16bbee85e5d573bcd6
xiaotang2333333 <59103928+xiaotang2333333@users.noreply.github.com>
2025-12-17 13:37:29 +0000

    12/17

--- linux.cmake
+++
@@ -1,2 +0,0 @@
-set(VCPKG_TARGET_TRIPLET x64-linux-dynamic)
-set(VCPKG_HOST_TRIPLET x64-linux-dynamic)
--- x64-arm64-toolchain.cmake
+++ x64-arm64-toolchain.cmake
@@ -6,12 +6,5 @@ set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv8-a")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv8-a")
-set(CMAKE_SYSROOT /usr/aarch64-linux-gnu)
 
 set(VCPKG_TARGET_TRIPLET arm64-linux-dynamic)
-
-if(VCPKG_CROSSCOMPILING)
-    set(ENV{PKG_CONFIG_SYSROOT_DIR} "${CMAKE_SYSROOT}")
-    set(ENV{PKG_CONFIG_LIBDIR} "${CMAKE_SYSROOT}/usr/lib/pkgconfig:${CMAKE_SYSROOT}/usr/share/pkgconfig")
-    set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_LIBDIR}")
-endif()
