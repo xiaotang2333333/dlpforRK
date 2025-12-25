4c66009046d213aae350b219f09621df4ae08b74
xiaotang2333333 <657177648@qq.com>
2025-12-25 15:53:53 +0800

    add flash device parameters file to binary directory and install it

---
+++ cmake/generate_intstall_qt_conf.cmake
@@ -0,0 +1,3 @@
+file(MAKE_DIRECTORY "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}")
+file(WRITE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/qt.conf"
+  "[Paths]\nPlugins=../Qt6/plugins\nFont=/usr/share/fonts")
